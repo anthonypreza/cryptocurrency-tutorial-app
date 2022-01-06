de5db8b07f97a8172c966c49fb7fda14e7c47cd5
Anthony Preza <apreza@netflix.com>
2022-01-05 23:48:03 -0800

    :sparkles: feat: approve and transferFrom

--- contracts/Juturna.sol
+++ contracts/Juturna.sol
@@ -5,21 +5,27 @@ contract Juturna {
     string public name = "Juturna";
     string public symbol = "JUT";
     string public standard = "Juturna v1.0";
+    uint256 public decimals = 18;
     uint256 public totalSupply;
 
-    // Transfer event
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) {
         totalSupply = _initialSupply;
 
-        // allocate the initial supply
         balanceOf[msg.sender] = totalSupply;
     }
 
-    // Transfer
     function transfer(address _to, uint256 _value)
         public
         returns (bool success)
@@ -35,6 +41,47 @@ contract Juturna {
 
         // emit the transfer event
         emit Transfer(msg.sender, _to, _value);
+
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // check if the sender has enough balance
+        require(balanceOf[msg.sender] >= _value);
+
+        // add the amount to the allowance
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        // check if the sender has enough balance
+        require(balanceOf[_from] >= _value);
+
+        // check if the allowance is big enough
+        require(allowance[_from][msg.sender] >= _value);
+
+        // subtract the amount from the sender
+        balanceOf[_from] -= _value;
+
+        // subtract the amoount from the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // add the amount to the recipient
+        balanceOf[_to] += _value;
+
+        // emit the transfer event
+        emit Transfer(_from, _to, _value);
 
         return true;
     }
--- test/Juturna.test.js
+++ test/Juturna.test.js
@@ -5,15 +5,12 @@ contract("Juturna", (accounts) => {
     const instance = await Juturna.deployed();
 
     const name = await instance.name();
-
     assert.equal(name, "Juturna", "name is correct");
 
     const symbol = await instance.symbol();
-
     assert.equal(symbol, "JUT", "symbol is correct");
 
     const standard = await instance.standard();
-
     assert.equal(standard, "Juturna v1.0", "standard is correct");
 
     return instance;
@@ -23,11 +20,9 @@ contract("Juturna", (accounts) => {
     const instance = await Juturna.deployed();
 
     const totalSupply = await instance.totalSupply();
-
     assert.equal(totalSupply.toNumber(), 1000000, "total supply is correct");
 
     const adminBalance = await instance.balanceOf(accounts[0]);
-
     assert.equal(adminBalance.toNumber(), 1000000, "admin balance is correct");
 
     return instance;
@@ -39,7 +34,6 @@ contract("Juturna", (accounts) => {
     const success = await instance.transfer.call(accounts[1], 250000, {
       from: accounts[0],
     });
-
     assert.equal(success, true, "it returns true");
 
     try {
@@ -54,7 +48,6 @@ contract("Juturna", (accounts) => {
     const receipt = await instance.transfer(accounts[1], 250000, {
       from: accounts[0],
     });
-
     assert.equal(receipt.logs.length, 1, "triggers one event");
     assert.equal(
       receipt.logs[0].event,
@@ -78,7 +71,6 @@ contract("Juturna", (accounts) => {
     );
 
     const balance = await instance.balanceOf(accounts[1]);
-
     assert.equal(
       balance.toNumber(),
       250000,
@@ -86,7 +78,6 @@ contract("Juturna", (accounts) => {
     );
 
     const senderBalance = await instance.balanceOf(accounts[0]);
-
     assert.equal(
       senderBalance.toNumber(),
       750000,
@@ -94,5 +85,133 @@ contract("Juturna", (accounts) => {
     );
 
     return instance;
+  });
+
+  it("approves tokens for delegated transfer", async () => {
+    const instance = await Juturna.deployed();
+
+    const success = await instance.approve.call(accounts[1], 100);
+    assert.equal(success, true, "it returns true");
+
+    try {
+      await instance.approve.call(accounts[1], 999999999999999);
+    } catch (e) {
+      assert(
+        e.message.indexOf("revert") >= 0,
+        "error message must contain revert"
+      );
+    }
+
+    const receipt = await instance.approve(accounts[1], 100, {
+      from: accounts[0],
+    });
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    assert.equal(
+      receipt.logs[0].event,
+      "Approval",
+      'should be the "Approval" event'
+    );
+    assert.equal(
+      receipt.logs[0].args._owner,
+      accounts[0],
+      "logs the account the tokens are authorized by"
+    );
+    assert.equal(
+      receipt.logs[0].args._spender,
+      accounts[1],
+      "logs the account the tokens are approved for"
+    );
+    assert.equal(receipt.logs[0].args._value, 100, "logs the approved amount");
+
+    const allowance = await instance.allowance(accounts[0], accounts[1]);
+    assert.equal(allowance.toNumber(), 100, "stores the allowance");
+
+    return instance;
+  });
+
+  it("handles delegated token transfers", async () => {
+    const instance = await Juturna.deployed();
+    const fromAccount = accounts[2];
+    const toAccount = accounts[3];
+    const spender = accounts[4];
+
+    // Transfer some tokens to fromAccount
+    await instance.transfer(fromAccount, 100, {
+      from: accounts[0],
+    });
+
+    // Approve spender to spend 10 tokens from fromAccount
+    await instance.approve(spender, 10, { from: fromAccount });
+
+    // Try transferring something larger than the sender's balance
+    try {
+      await instance.transferFrom(fromAccount, toAccount, 9999, {
+        from: spender,
+      });
+    } catch (e) {
+      assert(
+        e.message.indexOf("revert") >= 0,
+        "cannot transfer value larger than balance"
+      );
+    }
+
+    // Try transferring something larger than the approved amount
+    try {
+      await instance.transferFrom(fromAccount, toAccount, 20, {
+        from: spender,
+      });
+    } catch (e) {
+      assert(
+        e.message.indexOf("revert") >= 0,
+        "cannot transfer value larger than approved amount"
+      );
+    }
+
+    // Confirm successful transfer
+    const success = await instance.transferFrom.call(
+      fromAccount,
+      toAccount,
+      10,
+      {
+        from: spender,
+      }
+    );
+    assert.equal(success, true, "transfer successul");
+
+    // Transfer 10 tokens to toAccount using fromAccount's approved spender
+    const receipt = await instance.transferFrom(fromAccount, toAccount, 10, {
+      from: spender,
+    });
+    assert.equal(receipt.logs.length, 1, "triggers one event");
+    assert.equal(
+      receipt.logs[0].event,
+      "Transfer",
+      'should be the "Transfer" event'
+    );
+    assert.equal(
+      receipt.logs[0].args._from,
+      fromAccount,
+      "logs the account the tokens are transferred from"
+    );
+    assert.equal(
+      receipt.logs[0].args._to,
+      toAccount,
+      "logs the account the tokens are transferred to"
+    );
+    assert.equal(receipt.logs[0].args._value, 10, "logs the transfer amount");
+
+    const balance = await instance.balanceOf(toAccount);
+    assert.equal(
+      balance.toNumber(),
+      10,
+      "adds the amount to the receiving account"
+    );
+
+    const senderBalance = await instance.balanceOf(fromAccount);
+    assert.equal(
+      senderBalance.toNumber(),
+      90,
+      "deducts the amount from the sending account"
+    );
   });
 });
